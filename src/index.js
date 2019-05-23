4924a8f296c4c96fa1ff7e7954499d3ff865137e
Shinoda <julio.shinoda@digipix.com.br>
2019-05-23 19:07:52 -0300

    wip

---
+++ src/index.js
@@ -0,0 +1,26 @@
+const { GraphQLServer } = require('graphql-yoga')
+const { prisma} = require('./generated/prisma-client')
+
+const resolvers = {
+    Query: {
+        info: () =>  `This is teste`,
+        feed: (root,args, context,info) => {
+            return context.prisma.links()
+        },
+    },
+    Mutation: {
+        post: (root,args,context) => {
+            return context.prisma.createLink({
+                url: args.url,
+                description: args.description,
+            })
+        }
+    }
+}
+
+const server = new GraphQLServer({
+    typeDefs: './src/schema.graphql',
+    resolvers,
+    context:{prisma},
+})
+server.start(()=> console.log(`Server is runningn on 4000`))
